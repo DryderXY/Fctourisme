6d23bf2e4ec6422f1397786a18a9a1691bc2d4c8
Gaheriet Pataky <gaheriet.pataky@gmail.com>
2023-01-04 09:33:52 +0100

    Ajout user

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create("fr_FR");
+
+        $user = new User();
+        $user->setPrenom($faker->name);
+        $user->setNom($faker->lastName);
+        $user->setPseudo($faker->userName);
+        $user->setEmail($faker->email);
+        $user->setActif($faker->numberBetween(0,1));
+        $user->setCreatedAt($faker->dateTimeBetween('-6 months'));
+        $user->setPassword(md5("gaheriet"));
+        $user->setRoles(["ROLE_USER"]);
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
